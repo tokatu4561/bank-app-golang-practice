303d0aaae38c8b4bdf5fd4d71dfaba3eb4b74037
tokatu4561 <ryobsk4561@gmail.com>
2023-04-29 17:32:17 +0900

    deploy ecs and session use refreshtoken

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/tokatu4561/simple-bank/db/sqlc"
 )
 
@@ -23,9 +25,15 @@ func NewSever(store db.Store) (*Server, error) {
 func (server *Server) setupRouter() {
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+	
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 }
